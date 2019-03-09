df6fb9f9a58c96128d4fd69f52dc3657f5f85bb6
sambitmoh <sam.mohanty98@gmail.com>
2019-03-09 21:40:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// operator precedence 
+var now = 2018
+var fullage = 18;
+var yearjohn = 1989;
+var isfullage = now - yearjohn >= fullage;
+console.log(isfullage);
+var agejohn= now - yearjohn;
+var agemark = 35
+var avrage = (agejohn + agemark)/2;
+console.log(avrage);
+
+var x, y;
+x = y = (3+5) *4 - 6
+console.log(x ,y);
+
+x*=2
+console.log(x);
